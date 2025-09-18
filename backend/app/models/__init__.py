1cdb8f2195362f72c914e45ac08a628c79a0526f
A-tndn <tandon5787@gmail.com>
2025-09-18 23:22:10 +0530

    Add backend/app/models/__init__.py

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,21 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+# Import all models here for easy access
+from .user import User, SubscriptionTier
+from .social_accounts import SocialAccount, SocialPlatform  
+from .automation import Automation, AutomationStatus
+from .content import ContentQueue, ContentStatus
+from .analytics import AnalyticsDaily
+from .background_jobs import BackgroundJob, JobStatus
+
+__all__ = [
+    'db',
+    'User', 'SubscriptionTier',
+    'SocialAccount', 'SocialPlatform', 
+    'Automation', 'AutomationStatus',
+    'ContentQueue', 'ContentStatus',
+    'AnalyticsDaily',
+    'BackgroundJob', 'JobStatus'
+]
